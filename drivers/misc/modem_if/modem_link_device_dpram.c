e20640f05688ce9407ac1789d88b0619a02d49e7
Prashant Somashekar <prashsomash@gmail.com>
2013-06-17 19:53:14 -0400

    Revert "drivers: modem_if: use memcpy in dpram_download for header"
    -limiting this to only P4LTE
    
    This reverts commit 0dc15f580424db4d11928e0fcc1e72e081cba444.

--- drivers/misc/modem_if/modem_link_device_dpram.c
+++ drivers/misc/modem_if/modem_link_device_dpram.c
@@ -1278,7 +1278,11 @@ static int dpram_download
 		header.curframe = curframe;
 		header.len = plen;
 
+#ifdef CONFIG_ARCH_TEGRA
+		_memcpy(dest, &header, sizeof(header));
+#else
 		memcpy(dest, &header, sizeof(header));
+#endif
 		dest += sizeof(header);
 
 #ifdef CONFIG_ARCH_TEGRA
